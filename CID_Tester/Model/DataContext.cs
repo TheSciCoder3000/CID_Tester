4bb00a09ef02f7225a1d802f391e761a6b60d45c
Zvckkk <144102123+Zvckkk@users.noreply.github.com>
2024-12-18 12:49:21 +0800

    established the connection of the database to the wpf

--- CID_Tester/Model/DataContext.cs
+++ CID_Tester/Model/DataContext.cs
@@ -11,10 +11,26 @@ namespace CID_Tester.Model
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source =localhost;Initial Catalog=master;Integrated Security=True;Encrypt=False;Trust Server Certificate=False")
+            optionsBuilder.UseSqlServer("Data Source =ACE\\SQLEXPRESS;Initial Catalog=CID_TESTER;Integrated Security=True;Encrypt=False;Trust Server Certificate=False");
 
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TEST_PARAMETER>()
+                .HasKey(tp => tp.PARAM_CODE); // Explicitly define the primary key
+            modelBuilder.Entity<DUT>()
+                .HasKey(tp => tp.DUT_CODE);
+            modelBuilder.Entity<TEST_USER>()
+                .HasKey(tp => tp.USER_CODE);
+            modelBuilder.Entity<TEST_PROCEDURE>()
+                .HasKey(tp => tp.TEST_CODE);
+        }
         public DbSet<DUT> DUT { get; set; }
+        public DbSet<TEST_PARAMETER> TEST_PARAMETER { get; set; }
+        public DbSet<TEST_USER> TEST_USER { get; set; }
+        public DbSet<TEST_PROCEDURE> TEST_PROCEDURE { get; set; }
+
+        
     }
 }
