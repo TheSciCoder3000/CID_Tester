c83c651939c9bc8a4d5c15b7691238cd5eb06ae8
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2024-12-28 14:24:18 +0800

    moving DTOs from dbcontext folder to model folder

--- CID_Tester/Model/DTO/UserDTO.cs
+++ CID_Tester/Model/DTO/UserDTO.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CID_Tester.DbContexts.DTO
+namespace CID_Tester.Model.DTO
 {
     public class UserDTO
     {
-        
+
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int USER_CODE { get; set; }
@@ -18,23 +13,23 @@ namespace CID_Tester.DbContexts.DTO
         [Required]
         [Column(TypeName = "nvarchar(50)")]
         public string FIRST_NAME { get; set; } = null!;
-        
+
         [Required]
         [Column(TypeName = "nvarchar(50)")]
         public string LAST_NAME { get; set; } = null!;
-        
+
         [Required]
         [Column(TypeName = "nvarchar(150)")]
         public string EMAIL { get; set; } = null!;
-        
+
         [Required]
         [Column(TypeName = "nvarchar(50)")]
         public string PROFILE_IMAGE { get; set; } = null!;
-        
+
         [Required]
         [Column(TypeName = "nvarchar(50)")]
         public string USER_NAME { get; set; } = null!;
-        
+
         [Required]
         [Column(TypeName = "nvarchar(50)")]
         public string PASSWORD { get; set; } = null!;
