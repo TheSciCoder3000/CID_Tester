f703d396abff336b20fd9588a9724d4a17493100
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-01-03 12:09:29 +0800

    relocating interfaces, style selectors, and windows to their sub directories in view model

---
+++ CID_Tester/ViewModel/StyleSelectors/ItemStyleSelector.cs
@@ -0,0 +1,23 @@
+﻿using AvalonDock.Controls;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CID_Tester.ViewModel.StyleSelectors;
+
+public class ItemStyleSelector : StyleSelector
+{
+    public Style DocumentStyle { get; set; }
+    public Style AnchorableStyle { get; set; }
+
+    public override Style SelectStyle(object item, DependencyObject container)
+    {
+
+        if (container is LayoutAnchorableItem)
+            return AnchorableStyle;
+
+        if (container is LayoutDocumentItem)
+            return DocumentStyle;
+
+        return base.SelectStyle(item, container);
+    }
+}
---
+++ CID_Tester/ViewModel/Windows/AddDutViewModel.cs
@@ -0,0 +1,59 @@
+﻿using CID_Tester.ViewModel.Command;
+using CID_Tester.Model;
+using CID_Tester.ViewModel.Interfaces;
+using System.Windows.Input;
+
+namespace CID_Tester.ViewModel.Windows;
+
+public class AddDutViewModel : BaseViewModel, IDocument
+{
+    private readonly Action _closeDialog;
+
+    private readonly Store _AppStore;
+    private string _dutName = null!;
+    public string DutName
+    {
+        get { return _dutName; }
+        set
+        {
+            _dutName = value;
+            onPropertyChanged(nameof(DutName));
+        }
+    }
+
+    private string _dutDescription = null!;
+    public string DutDescription
+    {
+        get { return _dutDescription; }
+        set
+        {
+            _dutDescription = value;
+            onPropertyChanged(nameof(DutDescription));
+        }
+    }
+
+    public ICommand AddDutCommand { get; }
+
+    public string Title { get; }
+
+    public ICommand CloseCommand { get; }
+
+    public AddDutViewModel(Store appStore)
+    {
+        Title = "Add Devices";
+        _AppStore = appStore;
+        AddDutCommand = new RelayCommand(CreateDutHandler);
+        CloseCommand = new RelayCommand(RemoveAnchorable);
+    }
+
+    private void RemoveAnchorable(object? obj)
+    {
+        _AppStore.RemoveAnchorable(this);
+    }
+
+    private async void CreateDutHandler(object? obj)
+    {
+        await _AppStore.CreateDut(new DUT(DutName, DutDescription));
+        //_closeDialog.Invoke();
+    }
+}
