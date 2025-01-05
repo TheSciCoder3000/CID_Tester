fed1e2cda26f48fb768337f5e8df37fe16b1a420
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-01-05 14:11:01 +0800

    isolating Navigation commands

---
+++ CID_Tester/ViewModel/Command/Navigation/NavigateDevices.cs
@@ -0,0 +1,14 @@
+﻿using CID_Tester.Model;
+
+namespace CID_Tester.ViewModel.Command.Navigation;
+
+public class NavigateDevices(Store appStore) : CommandBase
+{
+    private readonly Store _AppStore = appStore;
+    private readonly DevicesViewModel _devicesView = new DevicesViewModel(appStore);
+
+    public override void Execute(object? parameter)
+    {
+        _AppStore.AddDocument(_devicesView);
+    }
+}
---
+++ CID_Tester/ViewModel/Command/Navigation/NavigateTestPlan.cs
@@ -0,0 +1,14 @@
+﻿using CID_Tester.Model;
+
+namespace CID_Tester.ViewModel.Command.Navigation;
+
+public class NavigateTestPlan(Store appStore) : CommandBase
+{
+    private readonly Store _AppStore = appStore;
+    private readonly TestPlanViewModel _testPlanView = new TestPlanViewModel(appStore);
+
+    public override void Execute(object? parameter)
+    {
+        _AppStore.AddDocument(_testPlanView);
+    }
+}
