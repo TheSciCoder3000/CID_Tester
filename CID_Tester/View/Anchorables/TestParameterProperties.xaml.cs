a7a8a34ca5d0d80c4ee1c678ebf8b35aa47f07f8
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-01-23 18:56:27 +0800

    Moving xaml document files to View/Document

---
+++ CID_Tester/View/Anchorables/TestParameterProperties.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CID_Tester.View.Anchorables;
+
+public partial class TestParameterProperties : UserControl
+{
+    public TestParameterProperties()
+    {
+        InitializeComponent();
+    }
+
+    private void TextBox_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            var textBox = sender as TextBox;
+            if (textBox != null)
+            {
+                var bindingExpression = textBox.GetBindingExpression(TextBox.TextProperty);
+                bindingExpression?.UpdateSource();
+                Keyboard.ClearFocus();
+            }
+        }
+    }
+}
---
+++ CID_Tester/View/Document/TestPlanView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace CID_Tester.View.Document;
+
+public partial class TestPlanView : UserControl
+{
+    public TestPlanView()
+    {
+        InitializeComponent();
+    }
+}
