25eaf9c9581a35d58ae7975a8f69403c67dee570
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-01-25 17:29:54 +0800

    isolated icons for color reactivity

---
+++ CID_Tester/View/Icons/Pause.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CID_Tester.View.Icons;
+
+public partial class Pause : UserControl
+{
+    public Pause()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly DependencyProperty CommandProperty =
+        DependencyProperty.Register("Command", typeof(ICommand), typeof(Pause), new PropertyMetadata(null));
+
+    public ICommand Command
+    {
+        get => (ICommand)GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+}
