a6570a8feac38995bf843a94cb99b4997055c7ae
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-03-03 22:29:36 +0800

    register ui with redirection to login and vice versa

---
+++ CID_Tester/Register.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace CID_Tester;
+public partial class Register : Window
+{
+    public Register()
+    {
+        InitializeComponent();
+    }
+
+    private void Exit_Click(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+
+    private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left)
+            this.DragMove();
+    }
+
+    private void MediaElement_MediaEnded(object sender, RoutedEventArgs e)
+    {
+        bg.Position = TimeSpan.FromMilliseconds(1);
+    }
+}
