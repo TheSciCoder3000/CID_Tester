757d5a7bc44219ab02f8d7f0587e24264d75f841
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-03-26 10:51:21 +0800

    history initial commit

---
+++ CID_Tester/ViewModel/Command/Navigation/NavigateHistory.cs
@@ -0,0 +1,15 @@
+﻿
+using CID_Tester.Store;
+using CID_Tester.ViewModel.Document;
+
+namespace CID_Tester.ViewModel.Command.Navigation;
+
+public class NavigateHistory(AppStore appStore) : CommandBase
+{
+    private readonly AppStore _AppStore = appStore;
+
+    public override void Execute(object? parameter)
+    {
+        _AppStore.DocumentStore.AddDocument<HistoryViewModel>(new HistoryViewModel(appStore));
+    }
+}
---
+++ CID_Tester/ViewModel/Document/HistoryViewModel.cs
@@ -0,0 +1,32 @@
+﻿
+using CID_Tester.Model;
+using CID_Tester.Store;
+using CID_Tester.ViewModel.Command;
+using System.Windows.Input;
+
+namespace CID_Tester.ViewModel.Document;
+
+public class HistoryViewModel : BaseViewModel
+{
+    private readonly AppStore _AppStore;
+
+    public string Title { get; set; }
+    public ICommand CloseCommand { get; }
+
+    public HistoryViewModel(AppStore appStore)
+    {
+        _AppStore = appStore;
+        Title = "History";
+        CloseCommand = new RelayCommand(CloseCommandHanlder);
+    }
+
+    public ICollection<TEST_BATCH> Batches
+    {
+        get => _AppStore.TestUser.TEST_BATCHES;
+    }
+
+    private void CloseCommandHanlder(object? obj)
+    {
+        _AppStore.DocumentStore.RemoveDocument(this);
+    }
+}
