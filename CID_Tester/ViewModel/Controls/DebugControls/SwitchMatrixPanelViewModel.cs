d28e8bf469e1005032e8a7f5895a3cdcf5ed9cfa
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-03-30 13:52:18 +0800

    initial collab commit

---
+++ CID_Tester/ViewModel/Controls/DebugControls/SwitchMatrixPanelViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CID_Tester.ViewModel.Controls.DebugControls;
+
+public class SwitchMatrixPanelViewModel
+{
+    public ICommand ToggleDut1Command { get; }
+    public ICommand ToggleDut2Command { get; }
+    public ICommand ToggleDut3Command { get; }
+    public ICommand ToggleDut4Command { get; }
+    public ICommand ToggleOffCommand { get; }
+
+    public IEnumerable<DebugParameterItemViewModel> ParameterItems { get; set; }
+}
