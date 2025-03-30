e9f84fc410a18c19ee93aaa7d91bd012b6b0a710
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-03-30 19:18:28 +0800

    disabling debug when no test plan selected

--- CID_Tester/ViewModel/Command/Navigation/NavigateDebug.cs
+++ CID_Tester/ViewModel/Command/Navigation/NavigateDebug.cs
@@ -5,14 +5,29 @@ using CID_Tester.Store;
 
 namespace CID_Tester.ViewModel.Command.Navigation;
 
-public class NavigateDebug(AppStore appStore, PS2000 oscilloscope, PS2000SigGen sigGen) : CommandBase
+public class NavigateDebug : CommandBase
 {
-    private readonly AppStore _AppStore = appStore;
+    private readonly AppStore _AppStore;
+    private readonly PS2000 _Oscilloscope;
+    private readonly PS2000SigGen _SigGen;
+
+    public NavigateDebug(AppStore appStore, PS2000 oscilloscope, PS2000SigGen sigGen)
+    {
+        _AppStore = appStore;
+        _Oscilloscope = oscilloscope;
+        _SigGen = sigGen;
+        _AppStore.TestPlanStore.OnTestPlanUpdated += (_) => OnCanExecuteChanged();
+    }
 
     public override void Execute(object? parameter)
     {
-        DebugViewModel viewModel = new DebugViewModel(appStore, oscilloscope, sigGen);
-        oscilloscope.DebugVM = viewModel;
+        DebugViewModel viewModel = new DebugViewModel(_AppStore, _Oscilloscope, _SigGen);
+        _Oscilloscope.DebugVM = viewModel;
         _AppStore.DocumentStore.AddDocument<DebugViewModel>(viewModel);
+    }
+
+    public override bool CanExecute(object? parameter)
+    {
+        return _AppStore.TestPlanStore.SelectedTestPlan != null && base.CanExecute(parameter);
     }
 }
